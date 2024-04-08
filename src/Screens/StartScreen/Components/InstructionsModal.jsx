fbec7cd1c49eb12d88dba4e85201a4161fea8f54
Minchan <minchanhan5@gmail.com>
2024-04-08 10:02:20 -0400

    updated instructions a bit

--- src/Screens/StartScreen/Components/InstructionsModal.jsx
+++ src/Screens/StartScreen/Components/InstructionsModal.jsx
@@ -12,38 +12,40 @@ function InstructionsModal({
   const badTeamStyle = {
     filter: 'invert(21%) sepia(76%) saturate(5785%) hue-rotate(338deg) brightness(57%) contrast(119%)'
   };
+  const goodTeamStyle = {
+    filter: 'invert(11%) sepia(92%) saturate(4093%) hue-rotate(234deg) brightness(92%) contrast(104%)'
+  };
 
   const objectiveTitle = "Objective";
-  const objective = `The Rebellion is a mafia style game where players need to debate and make decisions on "who the bad guys are". \
-  The Rebellion Team is good, Spy team is bad. `;
+  const objective = `The Rebellion is a mafia style game, consisting of a good team (The Rebellion) and a bad team (The Spies). The Rebellion \
+  is attempting to overthrow the government by passing missions (described below), but the secret spies are attempting to sabotage these missions.`;
 
   const pregameTitle = `Pregame/Board Info`;
-  const pregame = `When the room admin clicks Start, players will be put into the Rebellion or the Spies. The chart below shows how many
-  players are on each team based on total number of players. The Rebellion will not know any players' teams, but the spies will. \
-  Spies will be colored red.`
+  const pregame = `When the room admin clicks Start, players will be placed on the Rebellion or the Spy team. The chart below shows how many \
+  players are on each team based on total # of players. The Rebellion will not know any players' teams, and they will see other players as black fists. \
+  The Spies will see their own and fellow spies' fists as red, and the rebellions' as blue.`;
 
   const gameplayTitle = "Gameplay";
-  const gameplay = `At the start each round, a leader will be randomly chosen to choose a group of players to go on missions (star icon).
-  The leader (player with star) has X minutes to discuss and submit a team for vote (click on players and click "submit"). \
-  If they run out of time, the team will be "voted down", and a new leader is suggested.
+  const gameplay = `At the start of the game (mission 1), a leader will randomly be chosen. For each mission, the leader \
+  (player with star icon) has X minutes to discuss and submit a team for a mission (click on players and click "submit"). 
 
-  The number of players that go on each mission is indicated by the mission tracker on the table. \
-  Players will take a majority vote on if this group of players (checkbox icon) should go on the mission for that round. 
-  If the vote does not pass, then the leader to the right will suggest a new mission team.
+  The number of players that go on each mission is indicated by the mission trackers on the table (big circles). \
+  Players will take a majority vote on if this group of players (checkbox icon) should go on the mission for that round. \
+  If the vote does not pass (or the leader fails to submit a team in time), then the leader to the right will suggest a new mission team. \
   Note, the SPIES ALSO WIN if a mission team is voted against 5 times in one round (vote tracker is at the bottom of the board).
 
-  If a mission is approved, the rebellion players must "pass" the mission, but the spies have a choice to pass or fail the mission.
-  Players cannot see who voted what, they just see the tally once the mission concludes.
-  On most missions, just one fail is required to fail the entire mission (Mission 4 for 7+ player games require 2 FAILS).
-  Once the mission has ended, the leader to the right suggests a team for the next mission (clockwise).
+  If a mission is approved, the rebellion players must choose to "pass" the mission, but the spies have a choice to pass or fail the mission. \
+  Players cannot see who chose what, they just see the results once the mission concludes. \
+  On most missions, just one fail is required to fail the entire mission (Mission 4 for 7+ player games require 2 FAILS). \
+  Once the mission has ended, the next leader (moving in clockwise direction) suggests a team for the next mission.
 
-  Each game consists of 5 rounds (each with one mission) and the winning team is decided by best 3 out of 5. `;
+  Each game consists of 5 missions and the winning team is decided by best 3 out of 5. `;
 
   const additionalTitle = "Additional Notes";
   const additional = `- This game is best played over voice chat! Consider using a Discord server.
   - Remember, this web app is a rendition of an in-person board game. That means don't message any players privately, and try not to leave the \
-  game, as the game will end for everyone, just like normal.
-  - Please be careful when leaving your current tab if you're playing on a mobile device. Some browsers, including Chrome, have "memory saver" features \
+  game, as the game will end for everyone, just like in real life.
+  - Please be careful when leaving your current tab if you're playing on a mobile device. Some browsing apps, including Chrome, have "memory saver" features \
   that discard background tabs. You will be kicked from your room if your leave your tab.`;
 
   return (
@@ -108,7 +110,8 @@ function InstructionsModal({
                   <PlayerBox
                     isLeader={true}
                     onMission={true}
-                    username="rebellion(?)"
+                    teamStyle={goodTeamStyle}
+                    username="rebellion"
                   />
                 </div>
                 
@@ -117,7 +120,7 @@ function InstructionsModal({
                     isLeader={true}
                     onMission={true}
                     teamStyle={badTeamStyle} 
-                    username="spy member"
+                    username="spy"
                   />
                 </div>
                 
